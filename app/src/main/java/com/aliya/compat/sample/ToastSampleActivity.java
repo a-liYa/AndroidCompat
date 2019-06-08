a2748cb8e64678bcd48b8e3e53717020c588fceb
a_liYa <liyafei0412@126.com>
2019-06-08 12:46:00 +0800

    添加 Toast 异常演示

---
+++ app/src/main/java/com/aliya/compat/sample/ToastSampleActivity.java
@@ -0,0 +1,51 @@
+package com.aliya.compat.sample;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.aliya.compat.R;
+
+/**
+ * Toast WindowManager$BadTokenException 异常演示
+ *
+ * @author a_liYa
+ * @date 2019-06-08 12:11.
+ */
+public class ToastSampleActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView mTvTip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toast_sample);
+        setTitle("Toast演示");
+        mTvTip = findViewById(R.id.tv_tip);
+
+        mTvTip.setText("Toast异常需要系统在 Android 26 以下\n" + "当前 Api = " + Build.VERSION.SDK_INT);
+
+        findViewById(R.id.tv_normal).setOnClickListener(this);
+        findViewById(R.id.tv_exception).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_normal:
+                Toast.makeText(this, "我是正常Toast", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.tv_exception:
+                Toast.makeText(this, "我是异常Toast", Toast.LENGTH_LONG).show();
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    // ingore
+                }
+                break;
+        }
+    }
+}
